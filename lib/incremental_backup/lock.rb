6cf7e89ba0fe6c5fcf4a0aeb290305ae775ff4d9
Lasse Skindstad Ebert <lasseebert@gmail.com>
2013-01-31 10:17:54 +0100

    Lock

--- lib/incremental_backup/lock.rb
+++ lib/incremental_backup/lock.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 module IncrementalBackup
   class Lock
 
@@ -16,25 +18,29 @@ module IncrementalBackup
       end
     end
 
-    # Tries to obtain lock
+    # Obtains lock, run block, release lock
     def self.create task, &block
       lock = Lock.new task
       return false if lock.failed
-      yield
-      true
-    ensure
-      lock.release if lock
-    end
 
-    private
+      begin
+        yield
+      ensure
+        lock.release
+      end
+
+      true
+    end
 
     # Release lock
     def release
       FileUtils.rm path
     end
 
+    private
+
     def path
-      @path ||= File.join @task.settings.settings_path, LOCK_FILENAME.sub(/\{task_id\}/, @task.settins.task_id)
+      @path ||= File.join @task.settings.settings_path, LOCK_FILENAME.sub(/\{task_id\}/, @task.settings.task_id)
     end
   end
 end
