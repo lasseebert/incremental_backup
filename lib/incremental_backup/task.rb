b8f2d499417627ead8aa09d79537533fec21e87c
Lasse Skindstad Ebert <lasseebert@gmail.com>
2013-01-31 09:21:56 +0100

    Moved setting validation to run method, to not use logger in initializer

--- lib/incremental_backup/task.rb
+++ lib/incremental_backup/task.rb
@@ -11,13 +11,19 @@ module IncrementalBackup
     def initialize(&block)
       self.settings = TaskSettings.new
       yield settings
+    end
 
-      throw settings.errors.first unless settings.valid?
-
-      logger.info "Settings validated"
+    # Perform the backup
+    def run
+      validate_settings
     end
 
     private
+
+    def validate_settings
+      throw settings.errors.first unless settings.valid?
+      logger.info "Settings validated"
+    end
 
     def logger
       @logger ||= Logger.new(File.join(settings.settings_path, LOG_FILENAME), NUM_LOG_FILES)
